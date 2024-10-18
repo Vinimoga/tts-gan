5e86e1dec2a99778c75a353bbcfccdf3cbecc1d0
Vinimoga <vini.motta.garcia@gmail.com>
2024-10-18 01:14:03 +0000

    Daghar all 6 dataset training script tested

--- dagharGAN_Train.py
+++ dagharGAN_Train.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 import os
+import sys
 import argparse
+from colorama import init, Fore, Style
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -21,10 +23,10 @@ For Each experiment change:
     -exp_name : for the name of the experiment that will be saaved in log, along with the date
 '''
 
-max_iter = 5000
+max_iter = 50000
 checkpoint_number = 4
 seq_len = 30
-log_dir = 'logs/test'
+log_dir = 'logs/daghar_50000_30_100'
 
 default_string = f"CUDA_VISIBLE_DEVICES=0 python train_GAN_dahar.py -gen_bs 16 -dis_bs 16 \
                 --dist-url 'tcp://localhost:4321' --dist-backend 'nccl' --world-size 1 \
@@ -36,8 +38,22 @@ default_string = f"CUDA_VISIBLE_DEVICES=0 python train_GAN_dahar.py -gen_bs 16 -
                 --n_critic 1 --val_freq 20 --print_freq 100 --grow_steps 0 0 --fade_in 0 --patch_size 2 \
                 --ema_kimg 500 --ema_warmup 0.1 --ema 0.9999 --diff_aug translation,cutout,color"
 
-for class_name in [s.replace('.csv', '') for s in os.listdir('/workspaces/container-workspace/DAHAR_GANs')]:
+classes = os.listdir('DAGHAR_GANs')
+
+
+# Exemplo de uso no código
+print('\n Starting training', flush=True)
+print(f'Total of classes being trained: {len(classes)}\n', flush=True)
+print(classes, flush=True)
+print('----------------------------------------------------------------------------------------------------', flush=True)
+
+for class_name in [s.replace('.csv', '') for s in classes]:
     exp_name = f'{class_name}_{max_iter}_D_{seq_len}'
+    print('\n----------------------------------------------------------------------------------------------------', flush=True)
+    print('\n Starting individual training', flush=True)
+    print(f'{class_name} training', flush=True)
+    print('----------------------------------------------------------------------------------------------------', flush=True)
+
     os.system(f'{default_string}' + f' --class_name {class_name}' + f' --seq_len {seq_len}'\
           + f' --max_iter {max_iter}' + f' --exp_name {exp_name}' + f' --log_dir {log_dir}'\
           + f' --checkpoint_number {checkpoint_number}')
